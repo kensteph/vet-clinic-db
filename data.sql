4bd314b63ba5891a1ca602057ac0c630b176c48f
Kender ROMAIN <kenderromain@gmail.com>
2023-03-29 14:19:32 -0400

    Assign every owner his animals.

--- data.sql
+++ data.sql
@@ -32,3 +32,15 @@ INSERT INTO species (name) VALUES ('Digimon');
 -- All other animals are Pokemon
 UPDATE animals SET species_id=2 WHERE name LIKE '%mon';
 UPDATE animals SET species_id=1 WHERE name NOT LIKE '%mon';
+
+-- Modify your inserted animals to include owner information (owner_id):
+-- Sam Smith owns Agumon.
+-- Jennifer Orwell owns Gabumon and Pikachu.
+-- Bob owns Devimon and Plantmon.
+-- Melody Pond owns Charmander, Squirtle, and Blossom.
+-- Dean Winchester owns Angemon and Boarmon.
+UPDATE animals SET owner_id=1 WHERE name='Agumon';
+UPDATE animals SET owner_id=2 WHERE name IN('Gabumon','Pikachu');
+UPDATE animals SET owner_id=3 WHERE name IN('Devimon','Plantmon');
+UPDATE animals SET owner_id=4 WHERE name IN('Charmander','Squirtle','Blossom');
+UPDATE animals SET owner_id=5 WHERE name IN('Angemon','Boarmon');
